2a69c31ca0fdf71d8c81a70732fb062b1697eb24
Ravish Kumar <ravish99055@gmail.com>
2024-12-17 17:51:54 +0530

    Disable

--- script.js
+++ script.js
@@ -1,64 +1,81 @@
-const fromElement=document.getElementById("from");
-const toElement=document.getElementById("to");
-const temperatureInput=document.getElementById("temp1");
-const resultElement=document.getElementById('result');
+const fromElement = document.getElementById("from");
+const toElement = document.getElementById("to");
+const temperatureInput = document.getElementById("temp1");
+const resultElement = document.getElementById('result');
 
-function convertTemperature(event){
+function updateOptions() {
+    const fromValue = fromElement.value;
+    const toValue = toElement.value;
+    const fromOptions = Array.from(fromElement.options);
+    const toOptions = Array.from(toElement.options);
+
+    fromOptions.forEach(option => {
+        option.disabled = option.value === toValue;
+    });
+
+    toOptions.forEach(option => {
+        option.disabled = option.value === fromValue;
+    });
+    
+    if (fromElement.value === toValue) {
+        toElement.value = toOptions.find(option => !option.disabled).value;
+    }
+}
+
+function convertTemperature(event) {
     event.preventDefault();
-    const from=fromElement.value;
-    const to=toElement.value;
-    const temperature=parseFloat(temperatureInput.value);
+    const from = fromElement.value;
+    const to = toElement.value;
+    const temperature = parseFloat(temperatureInput.value);
 
-    if(isNaN(temperature)){
-        resultElement.innerText="Enter The Temperature";
+    if (isNaN(temperature)) {
+        resultElement.innerText = "Enter the Temperature";
         return;
     }
 
     let result;
-    if(from==="celsius"){
-        if(to==="fahrenheit"){
-            result = (temperature * 9/5)+32;
+    if (from === "celsius") {
+        if(to === "fahrenheit") {
+            result = (temperature * 9 / 5) + 32;
         } else if (to === "kelvin") {
-            result = temperature+273.15;
+            result = temperature + 273.15;
         }
-    } 
-    else if(from === "fahrenheit"){
-
-        if(to === "celsius"){
-            result=(temperature-32)*5/9;
-        } 
-        else if (to==="kelvin") {
-            result=(temperature-32)*5/9+273.15;
-        }
-    } else if (from==="kelvin") {
+    } else if (from === "fahrenheit") {
         if (to === "celsius") {
-            result = temperature-273.15;
-        } else if (to === "fahrenheit") {
-            result = (temperature-273.15)*9/5+32;
+            result = (temperature - 32) * 5 / 9;
+        } else if (to === "kelvin") {
+            result = (temperature - 32) * 5 / 9 + 273.15;
+        }
+    } else if (from === "kelvin") {
+        if (to === "celsius") {
+            result = temperature - 273.15;
+        } 
+        else if (to === "fahrenheit") {
+            result = (temperature - 273.15) * 9 / 5 + 32;
         }
     }
 
-    if (from===to) {
-        result=temperature;
-        resultElement.innerHTML=`Temperature is already in ${from}`
-    }
-
-    if(result !== undefined && from !== to){
-        resultElement.innerText=`Resultant Temperature: ${result.toFixed(2)}`;
-    }
+    resultElement.innerText = `Resultant Temperature: ${result.toFixed(2)}`;
 }
 
-function resetFields(){
-    fromElement.value='celsius';
-    toElement.value='fahrenheit';
-    temperatureInput.value='';
-    resultElement.innerText='';
+function resetFields() {
+    fromElement.value = 'celsius';
+    toElement.value = 'fahrenheit';
+    temperatureInput.value = '';
+    resultElement.innerText = '';
+    updateOptions();
 }
 
-fromElement.addEventListener("change", convertTemperature);
-toElement.addEventListener("change", convertTemperature);
+fromElement.addEventListener("change", () => {
+    updateOptions();
+    convertTemperature();
+});
+toElement.addEventListener("change", () => {
+    updateOptions();
+    convertTemperature();
+});
 temperatureInput.addEventListener("input", convertTemperature);
 
 const resetButton = document.getElementById('reset');
 resetButton.addEventListener("click", resetFields);
-
+updateOptions();
